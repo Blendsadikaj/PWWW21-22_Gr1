fb1c7848c91b93d414f475e5215dff4486e6ad23
unknown <blendsadikaj2@gmail.com>
2021-12-02 03:24:07 +0100

    added slider section

--- FirstPage.js
+++ FirstPage.js
@@ -6,8 +6,37 @@ window.addEventListener("click",function(){
     const items = document.querySelectorAll("header ul li a");
     items.forEach(item => {
         item.addEventListener("click", () => {
-            document.querySelector("a.active").classList.remove("active");
-            item.classList.add("active");
+            document.querySelector("a.activePage").classList.remove("activePage");
+            item.classList.add("activePage");
         })
     })
 })
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
