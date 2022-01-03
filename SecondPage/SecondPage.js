b7a2de837f3e84ff619a01389d97a6f44e86f8e3
unknown <blendsadikaj2@gmail.com>
2022-01-03 06:10:12 +0100

    added canvas drawing for signature

--- SecondPage/SecondPage.js
+++ SecondPage/SecondPage.js
@@ -10,29 +10,67 @@ function myFunction() {
         console.log(i.value);
         if(String.trim(i.getElementById("Name").value) === "")
             allAreFilled = false;
-        // if (!allAreFilled) return;
-        // if (!i.value) allAreFilled = false;
-        // console.log(i.value)
-        // if (i.value == null)
-        //     allAreFilled = false;
-        // if (i.type === "select-one") {
-        //     console.log("test")
-        //     var ddl = document.getElementById("selectGuest");
-        //     var selectedValue = ddl.options[ddl.selectedIndex].value;
-        //     if (selectedValue === "0") {
-        //         console.log("TEst")
-        //         allAreFilled = false;
-        //     }
-        // }
     }
     )
     if (!allAreFilled) {
         alert('Fill all the fields');
         return false;
     } 
-    // else {
-    //     console.log(allAreFilled);
-    //     document.getElementById("bookForm").submit();
-    //     return true;
-    // }
+}
+
+function showPopUp() {
+    document.getElementsByClassName('content')[0].style.filter = 'blur(4px)';
+    document.getElementById("signature").style.display = "block";
+  }
+
+
+let isDrawn = false;
+
+const paintCanvas = document.querySelector( '.js-paint' );
+const context = paintCanvas.getContext( '2d' );
+context.lineCap = 'round';
+
+const colorPicker = document.querySelector( '.js-color-picker');
+
+colorPicker.addEventListener( 'change', event => {
+    context.strokeStyle = event.target.value; 
+} );
+
+const lineWidthRange = document.querySelector( '.js-line-range' );
+const lineWidthLabel = document.querySelector( '.js-range-value' );
+
+context.lineWidth = 1;
+
+let x = 0, y = 0;
+let isMouseDown = false;
+
+const stopDrawing = () => { isMouseDown = false; }
+const startDrawing = event => {
+    isMouseDown = true;   
+   [x, y] = [event.offsetX, event.offsetY];  
+}
+const drawLine = event => {
+    if ( isMouseDown ) {
+        isDrawn = true;
+        const newX = event.offsetX;
+        const newY = event.offsetY;
+        context.beginPath();
+        context.moveTo( x, y );
+        context.lineTo( newX, newY );
+        context.stroke();
+        x = newX;
+        y = newY;
+    }
+}
+
+paintCanvas.addEventListener( 'mousedown', startDrawing );
+paintCanvas.addEventListener( 'mousemove', drawLine );
+paintCanvas.addEventListener( 'mouseup', stopDrawing );
+paintCanvas.addEventListener( 'mouseout', stopDrawing );
+
+function continueNow(){
+    if(isDrawn)
+        location.reload();
+    else
+        alert("You have to enter your signature");
 }
