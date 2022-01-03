8f08a4d01f3840a849bac1e0d2b0fbf23346039c
unknown <blendsadikaj2@gmail.com>
2022-01-03 05:18:13 +0100

    added login and signup with localStorage

--- FirstPage/FirstPage.js
+++ FirstPage/FirstPage.js
@@ -1,3 +1,5 @@
+var hidden = true;
+
 $(window).scroll(function(){
   var nav = $("nav");
   var headerTop =  nav.offset().top;
@@ -44,8 +46,65 @@ function showSlides(n) {
       slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].className = dots[i].className.replace("active", "");
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+}
+
+function login() {
+  document.getElementsByClassName('content')[0].style.filter = 'blur(4px)';
+  document.getElementById("login").style.display = "block";
+}
+
+function closeLogin() {
+  document.getElementById("login").style.display = "none";
+  document.getElementsByClassName('content')[0].style.filter = 'blur(0px)';
+}
+
+function signUp() {
+  document.getElementsByClassName('content')[0].style.filter = 'blur(4px)';
+  document.getElementById("sign-up").style.display = "block";
+}
+
+function closeSignUp() {
+  document.getElementById("sign-up").style.display = "none";
+  document.getElementsByClassName('content')[0].style.filter = 'blur(0px)';
+}
+
+function showAlert(action){
+  if(action === "login"){
+    if(sessionStorage.getItem("user") != null)
+      alert("You are already logged in")
+    else {
+      hidden = false;
+      sessionStorage.setItem("user",document.getElementById("emailLogin").value);
+    }
+      
+  }
+  else{
+    if(sessionStorage.getItem("user") != null)
+      alert("You are already logged in")
+    else sessionStorage.setItem("user",document.getElementById("email").value);
+  }
+    
+}
+
+function logout(){
+  sessionStorage.removeItem("user");
+  alert("You logged out");
+  location.reload();
+}
+
+window.onload = function(){
+  let personEmail = sessionStorage.getItem("user")
+  document.getElementById("username").innerHTML = personEmail;
+  if(sessionStorage.getItem("user") != null){
+    console.log("Test");
+    document.getElementById("logoutButton").style.display = "inline-block";
+  }
+  else{
+    console.log("Test");
+    document.getElementById("logoutButton").style.display = "none";
+  }
 }
