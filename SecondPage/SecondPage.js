a3be8842fcb4529726bf946fcf582f3c34d2a2de
unknown <blendsadikaj2@gmail.com>
2022-01-03 06:42:10 +0100

    added canvas image

--- SecondPage/SecondPage.js
+++ SecondPage/SecondPage.js
@@ -8,36 +8,49 @@ function myFunction() {
     console.log("teste");
     document.getElementById("bookForm").querySelectorAll("[required]").forEach(function (i) {
         console.log(i.value);
-        if(String.trim(i.getElementById("Name").value) === "")
+        if (String.trim(i.getElementById("Name").value) === "")
             allAreFilled = false;
     }
     )
     if (!allAreFilled) {
         alert('Fill all the fields');
         return false;
-    } 
+    }
 }
 
 function showPopUp() {
     document.getElementsByClassName('content')[0].style.filter = 'blur(4px)';
     document.getElementById("signature").style.display = "block";
-  }
+}
 
 
 let isDrawn = false;
 
-const paintCanvas = document.querySelector( '.js-paint' );
-const context = paintCanvas.getContext( '2d' );
+const paintCanvas = document.querySelector('.js-paint');
+const context = paintCanvas.getContext('2d');
 context.lineCap = 'round';
 
-const colorPicker = document.querySelector( '.js-color-picker');
+var grd = context.createLinearGradient(0, 0, 700, 580);
+grd.addColorStop(0, "#f1f1f1");
+grd.addColorStop(1, "white");
+context.fillStyle = grd;
+context.fillRect(0, 0, 600, 480);
 
-colorPicker.addEventListener( 'change', event => {
-    context.strokeStyle = event.target.value; 
-} );
+var img = document.getElementById("signatureExample")
+context.drawImage(img, 200, 0);
 
-const lineWidthRange = document.querySelector( '.js-line-range' );
-const lineWidthLabel = document.querySelector( '.js-range-value' );
+context.font = "30px Arial";
+context.fillStyle = "black";
+context.fillText("Example: ",20 , 100);
+
+const colorPicker = document.querySelector('.js-color-picker');
+
+colorPicker.addEventListener('change', event => {
+    context.strokeStyle = event.target.value;
+});
+
+const lineWidthRange = document.querySelector('.js-line-range');
+const lineWidthLabel = document.querySelector('.js-range-value');
 
 context.lineWidth = 1;
 
@@ -46,30 +59,30 @@ let isMouseDown = false;
 
 const stopDrawing = () => { isMouseDown = false; }
 const startDrawing = event => {
-    isMouseDown = true;   
-   [x, y] = [event.offsetX, event.offsetY];  
+    isMouseDown = true;
+    [x, y] = [event.offsetX, event.offsetY];
 }
 const drawLine = event => {
-    if ( isMouseDown ) {
+    if (isMouseDown) {
         isDrawn = true;
         const newX = event.offsetX;
         const newY = event.offsetY;
         context.beginPath();
-        context.moveTo( x, y );
-        context.lineTo( newX, newY );
+        context.moveTo(x, y);
+        context.lineTo(newX, newY);
         context.stroke();
         x = newX;
         y = newY;
     }
 }
 
-paintCanvas.addEventListener( 'mousedown', startDrawing );
-paintCanvas.addEventListener( 'mousemove', drawLine );
-paintCanvas.addEventListener( 'mouseup', stopDrawing );
-paintCanvas.addEventListener( 'mouseout', stopDrawing );
+paintCanvas.addEventListener('mousedown', startDrawing);
+paintCanvas.addEventListener('mousemove', drawLine);
+paintCanvas.addEventListener('mouseup', stopDrawing);
+paintCanvas.addEventListener('mouseout', stopDrawing);
 
-function continueNow(){
-    if(isDrawn)
+function continueNow() {
+    if (isDrawn)
         location.reload();
     else
         alert("You have to enter your signature");
